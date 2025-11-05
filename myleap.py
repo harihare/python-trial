98ed77b5b0b8fc93941384bf9991af3610a36877
Rajesh Sola <rajeshsola@gmail.com>
2025-11-05 22:39:34 +0530

    Correct typo and improve formatting in is_leap
    Fix typo in docstring and formatting in is_leap function.

--- myleap.py
+++ myleap.py
@@ -2,5 +2,5 @@
 
 
 def is_leap(yy):
-    """Functin to check given year is leap or not."""
-    return yy % 400 == 0  or (yy % 4 == 0 and yy % 100 != 0)
+  """Functin to check given year is leap or not."""
+  return yy % 400 == 0  or (yy % 4==0 and yy % 100 != 0)
